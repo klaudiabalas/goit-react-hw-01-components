70281c4465b3f99f3cc6d08890d42b14b4e02110
klaudiab <klaudiabalas1303@gmail.com>
2024-04-04 11:16:10 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { FriendList } from './Friend/Friend';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
-// import { TransactionHistory } from './Transactions/Transactions';
+import { TransactionHistory } from './Transactions/Transactions';
 
 import data from './DataJSON/data.json';
 import friends from './DataJSON/friends.json';
-// import transactions from './DataJSON/transactions.json';
+import transactions from './DataJSON/transactions.json';
 import user from './DataJSON/user.json';
 
 export const App = () => {
@@ -20,7 +20,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
-      {/* <TransactionHistory items={transactions} /> */}
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
